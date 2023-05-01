d132da91402c79d83a57bcc9fd4521ef3ba8e876
GabrielGhimici <ghimici.gabriel@gmail.com>
2023-05-01 22:51:30 +0300

    add deprecation of logic

---
+++ src/__deprecated/main.ts
@@ -0,0 +1,48 @@
+import { Canvas } from './core/canvas';
+import { checkWebGPU } from './utils/check-webgpu';
+import { renderTriangle } from './core/triangle';
+import { renderPrimitives } from './core/primitives';
+import { renderTrianglePrimitives } from './core/triangle-primitives';
+import { RenderContext, renderSquareWithBuffer } from './core/buffer-square';
+import { renderCube } from './core/cube/cube';
+
+export async function deprecatedRendering() {
+  const appContainer = document.getElementById('app');
+
+  const render = async (canvas: HTMLCanvasElement) => {
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+      throw new Error('Unable to retrieve adapter');
+    }
+    const device = await adapter.requestDevice();
+    const ctx = canvas.getContext('webgpu');
+    if (!ctx) {
+      throw new Error('Unable to retrieve canvas context!');
+    }
+    ctx.configure({
+      device: device,
+      format: navigator.gpu.getPreferredCanvasFormat(),
+      alphaMode: 'opaque',
+    });
+    // renderTriangle(ctx, device);
+    // renderPrimitives(ctx, device);
+    // renderPrimitives(ctx, device, 'line-list');
+    // renderPrimitives(ctx, device, 'line-strip');
+    // renderTrianglePrimitives(ctx, device);
+    // renderTrianglePrimitives(ctx, device, 'triangle-strip');
+    // renderSquareWithBuffer(ctx, device, RenderContext.TWO_BUFFERS);
+    // renderSquareWithBuffer(ctx, device, RenderContext.ONE_BUFFER);
+    // renderSquareWithBuffer(ctx, device, RenderContext.INDEXED);
+    renderCube(ctx, device);
+  };
+
+  if (appContainer) {
+    const canvas = new Canvas();
+    const htmlCanvas = canvas.element;
+    appContainer.appendChild(htmlCanvas);
+    checkWebGPU(htmlCanvas);
+    await render(htmlCanvas);
+  } else {
+    console.error('There is no app container present.');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,4 @@
 import '../index.css';
-import { Canvas } from './core/canvas';
-import { checkWebGPU } from './utils/check-webgpu';
-import { renderTriangle } from './core/triangle';
-import { renderPrimitives } from './core/primitives';
-import { renderTrianglePrimitives } from './core/triangle-primitives';
-import { RenderContext, renderSquareWithBuffer } from './core/buffer-square';
-import { renderCube } from './core/cube/cube';
+import { deprecatedRendering } from './__deprecated/main';
 
-const appContainer = document.getElementById('app');
-
-if (appContainer) {
-  const canvas = new Canvas();
-  const htmlCanvas = canvas.element;
-  appContainer.appendChild(htmlCanvas);
-  checkWebGPU(htmlCanvas);
-  await render(htmlCanvas);
-} else {
-  console.error('There is no app container present.');
-}
-
-async function render(canvas: HTMLCanvasElement) {
-  const adapter = await navigator.gpu.requestAdapter();
-  if (!adapter) {
-    throw new Error('Unable to retrieve adapter');
-  }
-  const device = await adapter.requestDevice();
-  const ctx = canvas.getContext('webgpu');
-  if (!ctx) {
-    throw new Error('Unable to retrieve canvas context!');
-  }
-  ctx.configure({
-    device: device,
-    format: navigator.gpu.getPreferredCanvasFormat(),
-    alphaMode: 'opaque',
-  });
-  // renderTriangle(ctx, device);
-  // renderPrimitives(ctx, device);
-  // renderPrimitives(ctx, device, 'line-list');
-  // renderPrimitives(ctx, device, 'line-strip');
-  // renderTrianglePrimitives(ctx, device);
-  // renderTrianglePrimitives(ctx, device, 'triangle-strip');
-  // renderSquareWithBuffer(ctx, device, RenderContext.TWO_BUFFERS);
-  // renderSquareWithBuffer(ctx, device, RenderContext.ONE_BUFFER);
-  // renderSquareWithBuffer(ctx, device, RenderContext.INDEXED);
-  renderCube(ctx, device);
-}
+deprecatedRendering();
